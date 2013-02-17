4d9787023de660d64dd058f0c25f6037a8d84c52
whitemaster <whthaker@gmail.com>
2013-02-18 01:18:57 +0400

    Update adminmanager/models.py

--- adminmanager/models.py
+++ adminmanager/models.py
@@ -7,7 +7,10 @@ from PIL import Image
 import urllib
 import os
 
-class Users(models.Model):
+class Users(models.Model): # 1) по большей части дублируется стандартная модель User, причём зря,
+			   #    отсюда вытекает невозможность использовать стандартные батарейки: 
+			   #    регистрацию, авторизацию, выход. Для них зачем-то написан велосипед, в добавок ко всему уязвимый, и это в системе хранения паролей! 
+			   # 2) Модель принято называть в единственном числе, т.к. она описывает объект а не группу 
 	login = models.CharField(max_length=30)
 	first_name = models.CharField(max_length=30)
 	second_name = models.CharField(max_length=30)
@@ -18,6 +21,17 @@ class Users(models.Model):
 
 	def __unicode__(self):
 		return self.login
+# Правильно это делается вот так:
+class Profile(models.Model):
+	user = models.ForeignKey(User, related_name='profile')
+	sex = models.BooleanField()
+	wishes = models.TextField(max_length=256)
+
+	def __unicode__(self):
+		return self.user.username
+# есть ещё вариант переопределить стандартную модель User
+# но тогда придётся править админку, и другие стандартные батарейки для работы с новой моделью, 
+# так что мне больше нравится первый вариант
 
 class CSVBox(models.Model):
 	title = models.CharField(max_length=30)
